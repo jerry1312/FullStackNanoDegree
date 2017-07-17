80374453f33242f87e554fc73a93c5d4624ef0d2
jerry1312 <kts8427185@gmail.com>
2017-07-17 20:46:26 +0530

    Create newshape.py

---
+++ newshape.py
@@ -0,0 +1,23 @@
+import turtle
+
+window=turtle.Screen()
+
+window.bgcolor("gray")
+
+brad=turtle.Turtle()
+brad.speed(0)
+
+def square():
+	for i in range(0,4,1):
+		brad.forward(100)
+		brad.left(90)
+def circle():
+	brad.circle(120)
+
+for i in range(1,100,1):
+	square()
+	circle()
+	brad.left(10)
+
+
+window.exitonclick()
